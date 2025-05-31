9f1d16b68a0614b3f4257cd8729c64f216438c8e
Matissoss <vateuszptaszynski@proton.me>
2025-05-31 18:07:48 +0200

    tests: created new test.sh, made a profile for testing purposes

--- tests/test.sh
+++ tests/test.sh
@@ -1,21 +1,38 @@
-#!/bin/bash
+#!/bin/sh
+
+NASM_BIN="nasm"
+RASM_BIN="rasm"
+BIN=.tmp/$RASM_BIN
+TESTING_PROFILE="testing"
+NASM_FLAGS="-Wno-prefix-seg -Wno-prefix-hle -Wno-label-orphan"
+RASM_FLAGS="-t"
+NASM_FILE_RES=.tmp/nasm-tmp.bin
+RASM_FILE_RES=.tmp/rasm-tmp.bin
+
+_=$($NASM_BIN -h)
+
+if [[ "$?" != "0" ]]; then
+	echo "Testing requires having installed NASM binary in PATH!"
+	exit
+fi
 
 set -e
 
-if [[ $1 == '-f' ]]; then
-	cargo fmt
-fi
-if [[ $1 == '-r' ]]; then
-	cd ..
-	./instradd.sh
-	cd tests
-fi
-
-NASM_FLAGS="-Wno-prefix-seg -Wno-prefix-hle -Wno-label-orphan"
-RASM_FLAGS="-t"
-
+echo "running checks..."
+cargo check -q
+echo "running tests..."
 cargo test -q
-cargo clippy
+echo "running clippy..."
+cargo clippy -q
+
+rm -rf .tmp
+mkdir .tmp
+
+cd ..
+echo "building tmp binary..."
+cargo build --profile $TESTING_PROFILE -q
+mv target/$TESTING_PROFILE/$RASM_BIN tests/.tmp/$RASM_BIN
+cd tests
 
 errors=0
 
@@ -23,45 +40,41 @@ for file in ./nasm/*.asm; do
 	NASM_FILE=$file
 	RASM_FILE=${file/nasm/rasm}
 
-	NASM_FILE_RES=${NASM_FILE/.asm/.bin}
-	RASM_FILE_RES=${RASM_FILE/.asm/.bin}
-
-	nasm $NASM_FILE -o $NASM_FILE_RES -f bin $NASM_FLAGS
-	cargo run -q -- -i=$RASM_FILE -o=$RASM_FILE_RES -f=bin $RASM_FLAGS
+	./.tmp/rasm -i=$RASM_FILE -o=$RASM_FILE_RES -f=bin -t
+	$NASM_BIN $NASM_FILE -o $NASM_FILE_RES -f bin $NASM_FLAGS
 	
-	NASM_RES=$(xxd $NASM_FILE_RES)
 	RASM_RES=$(xxd $RASM_FILE_RES)
+	NASM_RES=$(xxd $NASM_FILE_RES)
 
-	if [[ "$NASM_RES" != "$RASM_RES" ]]; then
-		echo ""
-		echo "--- SOURCE CODE ---"
-		cat $NASM_FILE
-		echo "-------------------"
-		cat $RASM_FILE
-		echo "-------------------"
-		echo "$NASM_FILE | $RASM_FILE"
-		echo "NASM HEX DUMP: "
-		xxd ${NASM_FILE/.asm/.bin}
-		echo "---"
-		echo "RASM HEX DUMP: "
-		xxd ${RASM_FILE/.asm/.bin}
-		echo "---"
-		errors=$((errors+1))
+	if [[ "$RASM_RES" != "$NASM_RES" ]]; then
+		echo "NASM HEX DUMP"
+		echo "-------------"
+		xxd $NASM_FILE_RES
+		echo "-------------"
+		echo "RASM HEX DUMP"
+		echo "-------------"
+		xxd $RASM_FILE_RES
+		echo "-------------"
+		errors=$((errors + 1))
 	fi
+
 	rm $NASM_FILE_RES
 	rm $RASM_FILE_RES
 done
 
+# reverts change to set -e
+set -e himBHse
+
 for file in ./elf/*.asm; do
-	rm -f main.o
-	cargo r -q -- -i=$file -o=main -f=elf64 $RASM_FLAGS
-	readelf_res=$(readelf -a "main" | grep -i "error:|warning:" || true)
+	rm -f .tmp/tmp.o
+	$BIN -i=$file -o=.tmp/tmp.o -f=elf64 $RASM_FLAGS
+	readelf_res=$(readelf -a ".tmp/tmp.o" | grep -i "error:|warning:")
 	if [[ $readelf_res != "" ]]; then
 		errors=$((errors+1))
 		echo "Invalid output in ${file}:"
-		readelf -a "main"
+		readelf -a ".tmp/tmp.o"
 	fi
-	rm main
+	rm .tmp/tmp.o
 done
 
 if [[ "$errors" == "0" ]]; then
