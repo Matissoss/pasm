d4b39e5afa162be7ed2cd2f9ea724212ef686671
Matissoss <matissossgamedev@proton.me>
2025-12-13 18:46:01 +0100

    more work on assemble function in libp

--- src/libp.rs
+++ src/libp.rs
@@ -3,12 +3,17 @@
 // made by matissoss
 // licensed under MPL 2.0
 
-use std::{fs::OpenOptions, io::Read, path::Path};
+use std::{error::Error, fs::OpenOptions, io::Read, path::Path};
 
 use crate::{
-    pre::par::{par, LineResult, ParserStatus},
-    shr::error::Error as PasmError,
-    utils::LineIter,
+    core::{
+        api::{self, AssembleResult}, comp
+    }, pre::{
+        chk, par::{par, LineResult, ParserStatus}
+    }, shr::{
+        error::Error as PasmError,
+        reloc::{RelType, Relocation},
+    }, utils::LineIter
 };
 
 pub fn assemble(ipath: &Path, opath: &Path) -> Result<(), PasmError> {
@@ -30,9 +35,11 @@ pub fn assemble(ipath: &Path, opath: &Path) -> Result<(), PasmError> {
     }
 
     // get output from assembler
-    let _obuf: Vec<u8> = Vec::new();
+    let mut obuf: Vec<u8> = Vec::new();
+    let mut rels: Vec<Relocation> = Vec::new();
     let mut line_iter = LineIter::new(&ibuf);
     let mut parser_status = ParserStatus::default();
+    let bits = 16;
     while let Some((lnum, line)) = line_iter.next() {
         let line = line.trim();
         match par(&mut parser_status, line) {
@@ -40,16 +47,51 @@ pub fn assemble(ipath: &Path, opath: &Path) -> Result<(), PasmError> {
                 e.set_line(lnum);
                 return Err(e);
             }
-            LineResult::Instruction(_i) => {
+            LineResult::Instruction(i) => {
+                let e = if bits == 64 {
+                    chk::check_ins64bit(i)
+                } else {
+                    chk::check_ins32bit(i)
+                };
+                if let Err(e) = e {
+                    e.set_line(lnum);
+                    return Err(e);
+                }
+                let (res, mut rel_a) = comp::get_genapi(i, bits)
+                    .assemble(i, bits, RelType::REL32);
+                for r in rel_a.iter_mut() {
+                    r.offset += obuf.len();
+                }
+                match res {
+                    AssembleResult::WLargeImm(d) => obuf.extend(d.into_iter()),
+                    AssembleResult::NoLargeImm(d) => obuf.extend(d),
+                }
+                rels.extend(rel_a.into_iter());
+            }
+            LineResult::Section(s) => {
                 todo!()
             }
-            LineResult::Section(_s) => {
-                todo!()
-            }
-            LineResult::Label(_l) => {
+            LineResult::Label(l) => {
                 todo!()
             }
             _ => {}
+        }
+    }
+
+    match parser_status.attributes.get("target").unwrap_or("elf64") {
+        #[cfg(feature = "target_elf")]
+        "elf64" | "ELF64" => {
+
+        },
+        #[cfg(feature = "target_elf")]
+        "elf32" | "ELF32" => {
+
+        },
+        "bin" => {
+
+        }
+        t => {
+            return Err(PasmError::new(format!("unknown target {t}"), todo!()))
         }
     }
 
